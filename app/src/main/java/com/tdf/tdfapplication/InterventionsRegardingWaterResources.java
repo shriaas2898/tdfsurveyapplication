43658dec6926dfc9d50b705280be10da2e542076
shriaas2898 <aasthashrivastava410@gmail.com>
2019-08-26 01:19:39 +0530

    added upstream

---
+++ app/src/main/java/com/tdf/tdfapplication/InterventionsRegardingWaterResources.java
@@ -0,0 +1,13 @@
+package com.tdf.tdfapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InterventionsRegardingWaterResources extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interventions_regarding_water_resources);
+    }
+}
