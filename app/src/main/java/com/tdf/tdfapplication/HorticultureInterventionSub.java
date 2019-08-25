720e951c6799104a9fcee18cbbdf0f5d93f45d4d
shriaas2898 <aasthashrivastava410@gmail.com>
2019-08-25 21:01:11 +0530

    added layout in horticutuar_intervention sub

---
+++ app/src/main/java/com/tdf/tdfapplication/HorticultureInterventionSub.java
@@ -0,0 +1,13 @@
+package com.tdf.tdfapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HorticultureInterventionSub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horticulture_intervention_sub);
+    }
+}
